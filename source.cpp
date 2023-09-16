0aaf8cf836c82960fb019f9ac48e8116bdfa1b25
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2023-09-16 09:53:55 -0400

    Added logic to place generated files in directory (currently ctil by default)

--- source.cpp
+++ source.cpp
@@ -1,9 +1,13 @@
 // ctil.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <string>
+
+namespace fs = std::filesystem;
 enum AppErrors
 {
 	CannotOpenFile = 1, // An input file cannot be opened
@@ -27,8 +31,14 @@ int main(int argc, char* argv[])
 	newName = ((std::string)argv[1]).substr(0, ((std::string)argv[1]).find(".txt")) + ".html";
 	//std::cout << "new filename is: " + newName;
 
+	// create destination directory (./ctil by default)
+	std::string dir = "ctil";
+	if (std::filesystem::exists(dir))
+		std::filesystem::remove_all(dir);
+	std::filesystem::create_directory(dir);
 	// open output file
-	std::ofstream outfile(newName);
+	std::string newFile = dir + "/" + newName;
+	std::ofstream outfile(newFile);
 	if (outfile.is_open())
 	{
 		outfile << "<!doctype html>" << std::endl
@@ -56,7 +66,7 @@ int main(int argc, char* argv[])
 	}
 	else
 	{
-		std::cerr << "ERROR: Cannot open file" << std::endl;
+		std::cerr << "ERROR: Cannot open file: " + newFile << std::endl;
 		exit(AppErrors::CannotOpenFile);
 	}
 
