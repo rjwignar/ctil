6565de8f88377940c6d680cf8704e6db72dec63b
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2023-09-17 13:54:23 -0400

    Added bugfixesin to generateHTML related to the following: - duplication of last line of input .txt file in generated HTML file - parsed content blocks ignored in HTML generation if at the end of the input .txt file with no newline afterwards

--- ctil.cpp
+++ ctil.cpp
@@ -19,7 +19,7 @@ namespace cdot
 		// entire parsed content to be placed inside <body>...</body> tags
 		std::string bodyContent;
 
-		while (infile)
+		while (infile && !infile.eof())
 		{
 			// for debug purposes - will modify in later versions
 			// have a content block string
@@ -30,8 +30,15 @@ namespace cdot
 			// check line content 
 			if (line != "" && newlineCounter == 0)
 				// if line is non-empty, and newLineCounter == 0, then add line to content
+
 			{
 				content += line + "\n";
+				if (infile.eof())
+				{
+					bodyContent += "<p>" + content + "</p>" + "\n";
+				}
+				//else
+				//	content += line + "\n";
 			}
 			else if (line != "" && newlineCounter == 1)
 				// if line is non-empty and newlineCounter == 1, then wrap  content in <p>...</p>, reset content to empty string, add line to content and keep going
@@ -43,14 +50,23 @@ namespace cdot
 				bodyContent += "<p>" + content + "</p>" + "\n";
 
 				// overwrite old content string with current contents of line
-				content = line;
+				content = line + "\n";
+
+				// 
+				newlineCounter = 0;
+
+				if (infile.eof())
+				{
+					bodyContent += "<p>" + content + "</p>" + "\n";
+				}
 			}
 			else if (line != "" && newlineCounter == 2)
 				// if line is non-empty and newlineCounter == 2, then follow title adjustment logic
 			{
 				titleParsed = true;
 				title = content;
-				content = line;
+				content = line + "\n";
+				newlineCounter = 0;
 			}
 			else if (line == "")
 				// else if the line is empty (""), then increment newline counter by 1
@@ -64,6 +80,7 @@ namespace cdot
 					newlineCounter++;
 				}
 			}
+
 			else
 			{
 				bodyContent += "<p>" + content + "</p>" + "\n";
