bbbb8f089be2f86fee389f46ebfa7250df9c60a9
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2023-09-17 00:18:40 -0400

    Began adding logic for when directory is passed as argument. Modfiied single-file processing logic to use the last command-line argument as the target insteaad of the second command-line argument. This was done to accomodate the addition of optional flags in the tool

--- source.cpp
+++ source.cpp
@@ -20,41 +20,49 @@ int main(int argc, char* argv[])
 {
 	std::cout << "Hello World!\n";
 
-	std::ifstream infile(argv[1]);
-	if (!infile)
+	// if argv[1]/ last argument is a directory
+	if (std::filesystem::is_directory(argv[argc - 1]))
 	{
-		std::cerr << "ERROR: Cannot open file" << std::endl;
-		exit(AppErrors::CannotOpenFile);
+		std::cout << "Argument is a directory" << std::endl;//remove later
 	}
-
-	// for now, assume argument is a file and print out line-by-line
-
-	// initialize ctil object
-	cdot::ctil ctil;
-
-	std::string newName;
-	newName = ((std::string)argv[1]).substr(0, ((std::string)argv[1]).find(".txt")) + ".html";
-	//std::cout << "new filename is: " + newName;
-
-	// create destination directory (./ctil by default)
-	std::string dir = "ctil";
-	if (std::filesystem::exists(dir))
-		std::filesystem::remove_all(dir);
-	std::filesystem::create_directory(dir);
-
-	// create newFile name using dir and newName
-	std::string newFile = dir + "/" + newName;
-	// open output file
-	std::ofstream outfile(newFile);
-	if (outfile.is_open())
+	else //logic for single text file
 	{
-		ctil.generateHTML(infile, outfile, newFile);
-		outfile.close();
-	}
-	else
-	{
-		std::cerr << "ERROR: Cannot open file: " + newFile << std::endl;
-		exit(AppErrors::CannotOpenFile);
+
+		std::ifstream infile(argv[argc - 1]);
+		if (!infile)
+		{
+			std::cerr << "ERROR: Cannot open file" << std::endl;
+			exit(AppErrors::CannotOpenFile);
+		}
+		// for now, assume argument is a file and print out line-by-line
+
+		// initialize ctil object
+		cdot::ctil ctil;
+
+		std::string newName;
+		newName = ((std::string)argv[1]).substr(0, ((std::string)argv[1]).find(".txt")) + ".html";
+		//std::cout << "new filename is: " + newName;
+
+		// create destination directory (./ctil by default)
+		std::string dir = "ctil";
+		if (std::filesystem::exists(dir))
+			std::filesystem::remove_all(dir);
+		std::filesystem::create_directory(dir);
+
+		// create newFile name using dir and newName
+		std::string newFile = dir + "/" + newName;
+		// open output file
+		std::ofstream outfile(newFile);
+		if (outfile.is_open())
+		{
+			ctil.generateHTML(infile, outfile, newFile);
+			outfile.close();
+		}
+		else
+		{
+			std::cerr << "ERROR: Cannot open file: " + newFile << std::endl;
+			exit(AppErrors::CannotOpenFile);
+		}
 	}
 
 	return 0;
