f1be3c22c704a395d8647341325ee51bdb23696b
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2023-09-17 22:17:23 -0400

    Started adding flag logic to Process module so program can accept and parse optional flag arguments. Current flags being worked on: -v (version), -h (help), -o (output - user-specified destination folder). As of this commit, the main method is unchaged, and Process module is still unused

--- Process.cpp
+++ Process.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <filesystem>
 #include "Process.h"
 
 namespace cdot
@@ -13,5 +15,69 @@ namespace cdot
 	{
 		return "<title>" + title + "</title>" + "\n"
 			+ "<h1>"+title+"</h1>" + "\n";
+	}
+
+	std::string Process::removeWhitespace(const char* str)
+	{
+		std::string strn(str);
+		strn.erase(std::remove_if(strn.begin(), strn.end(), ::isspace), strn.end());
+		return strn;
+	}
+	int cdot::allArguments(int argc, char* argv[])
+	{
+		cdot::Process processor;
+		int result = 0;
+		commands cmd{ commands::INVALID };
+
+		// process first argument and make sure it's valid by looking for valid commands or textfile/directory
+
+		// trim all whitespaces
+		std::string temp = processor.removeWhitespace(argv[1]);
+
+		// use for_each algorithm to convert argument to all lower case
+		std::for_each(temp.begin(), temp.end(), [](char& c)
+			{
+				c = ::tolower(c);
+			});
+
+		// --version flag enabled
+		if (temp == "-v" || temp == "--version")
+		{
+			cmd == commands::VERSION;
+		}
+		// --help flag enabled
+		else if (temp == "-h" || temp == "--help")
+		{
+			cmd == commands::HELP;
+		}
+		// --output flag enabled
+		else if (temp == "-o" || temp == "--output")
+		{
+			cmd == commands::OUTPUT;
+		}
+		// the first argument after ctil.exe is a file or directory
+		else if ((std::filesystem::exists(temp)))
+		{
+			cmd == commands::ITEM;
+		}
+		// invalid argument
+		else
+		{
+			cmd == commands::INVALID;
+		}
+
+		switch (cmd)
+		{
+		case commands::VERSION:
+			// display version information
+			break;
+		case commands::HELP:
+			// show usage information
+			break;
+		case commands::OUTPUT:
+			// follow output logic
+		case commands::ITEM:
+			// follow standard logic for directory/text file
+		}
 	}
 }
